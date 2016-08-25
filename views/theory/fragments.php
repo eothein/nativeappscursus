b33d7bb51e9b25df0ff32f175a6df3fb997b50cd
thibautvincent <thibautvincent@icloud.com>
2016-08-25 12:47:56 +0200

    Fix typo

--- views/theory/fragments.php
+++ views/theory/fragments.php
@@ -14,9 +14,9 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-sm-12">
             <h1>Fragments and best practises</h1>
 
-            <p>Before we go into some more concepts 
-                ww will take a quick review of  best practices learned from the Lessons  from 
-                last weeks.</p>  
+            <p>Before we go into some more concepts
+                we will take a quick review of best practices learned from the lessons from
+                last weeks.</p>
             <ul>
                 <li>Use Fragments to represent a UI screen</li>
                 <li>Use Activities just to manage Fragments</li>
